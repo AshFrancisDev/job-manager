02de26ca8d05351e07cd0b410af293992216b940
Ash Francis <39334343+AshFrancisDev@users.noreply.github.com>
2020-02-05 09:07:08 +0000

    vanity

--- job-manager.php
+++ job-manager.php
@@ -17,7 +17,7 @@
  * Plugin URI:        http://example.com/job-manager-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Ash Francis @ Yell
+ * Author:            Ash Francis @ Yell.com
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
